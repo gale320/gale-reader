01ce764df88e673c4b372e846e760ab3d0114f87
gale <gale320@gmail.com>
2013-10-16 20:16:03 +0800

    add glib and poppler

---
+++ third/libebook/ebook-tools/src/libepub/CMakeFiles/epub.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/epub.dir/epub.o"
+  "CMakeFiles/epub.dir/ocf.o"
+  "CMakeFiles/epub.dir/opf.o"
+  "CMakeFiles/epub.dir/linklist.o"
+  "CMakeFiles/epub.dir/list.o"
+  "../../libs/libepub.pdb"
+  "../../libs/libepub.so"
+  "../../libs/libepub.so.0.2.1"
+  "../../libs/libepub.so.0"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/epub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ third/libebook/ebook-tools/src/tools/CMakeFiles/einfo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/einfo.dir/einfo.o"
+  "../../bin/einfo.pdb"
+  "../../bin/einfo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/einfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
